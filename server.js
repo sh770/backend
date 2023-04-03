713373064449c7374b474a9b9d3179e4cbaa2979
sh <sh770h@gmail.com>
2023-04-03 22:33:06 +0300

    0304

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRoutes.js";
+import seedRouter from "./routes/seedRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 
 
-const app = express();
 dotenv.config();
 
 mongoose
@@ -19,10 +20,15 @@ mongoose
         console.log(err.message);
     });
 
+const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', productRouter);
+app.use('/api/seed', seedRouter);
+app.use('/api/users', userRouter);
+
 
 
 
